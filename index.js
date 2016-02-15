d0aa3c517977c4c493aea2f8bda650b5f54fc074
liuxin.rkl <liuxin.rkl@taobao.com>
2016-02-15 14:47:08 +0800

    fix index.js

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ if (global._asyncHook) {
   // In case the version match, we can simply return the first initialized copy
   if (global._asyncHook.version === require('./package.json').version) {
     module.exports = global._asyncHook;
-    return;
   }
   // The version don't match, this is really bad. Lets just throw
   else {
     throw new Error('Conflicting version of async-hook found');
   }
+} else {
+  module.exports = global._asyncHook = new AsyncHook();
 }
-
-module.exports = global._asyncHook = new AsyncHook();
